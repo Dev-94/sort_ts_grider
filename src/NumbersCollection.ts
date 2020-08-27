58da802d44176737808db452c2bf51c2e36029ac
Dev Govindji <friendsofdev94@gmail.com>
2020-08-27 15:00:17 -0500

    uses abstract t classes for different data types

--- src/NumbersCollection.ts
+++ src/NumbersCollection.ts
@@ -1,5 +1,9 @@
-export class NumbersCollection {
-    constructor(public data: number[]) { }
+import { Sorter } from './Sorter'
+
+export class NumbersCollection extends Sorter {
+    constructor(public data: number[]) {
+        super()
+    }
 
     // `get` means you dont have to invoke when you reference it
     get length(): number {
